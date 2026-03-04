ed42f9ac18f65020966cabe2677ee0a145dff914
gabriel <bielyoshida@gmail.com>
2026-03-04 14:42:03 -0300

    trabalho concluído

--- DADI-Trabalho-03-03/Trabalho 03-03/Trabalho 03-03/Program.cs
+++ DADI-Trabalho-03-03/Trabalho 03-03/Trabalho 03-03/Program.cs
@@ -13,16 +13,16 @@ namespace Trabalho_03_03
             List<Aluno> aluno = new List<Aluno>();
             int intTemporario = 0;
             float floatTemporario = 0;
+            float imc = 0;
             int i = 0;
             int j = 0;
 
-            while (i != 4)
+            while (i != 3)
             {
                 Console.WriteLine("\nMenu Academia");
                 Console.WriteLine("1 - Cadastrar aluno");
                 Console.WriteLine("2 - Listar alunos");
-                Console.WriteLine("3 - Mostrar IMC e classificação");
-                Console.WriteLine("4 - Sair");
+                Console.WriteLine("3 - Sair");
                 Console.Write("Digite a opção desejada: ");
                 
                 if(!int.TryParse(Console.ReadLine(), out i))
@@ -58,7 +58,24 @@ namespace Trabalho_03_03
                             Console.Write("Opção inválida, digite a altura do aluno: ");
                         }
                         a.altura = floatTemporario;
+
+                        aluno.Add(a);
+
                         Console.WriteLine("Aluno Cadastrado com sucesso");
+                        break;
+
+                    case 2:
+                        foreach (Aluno temporario in aluno)
+                        {
+                            imc = IMC.calcular(temporario.peso, temporario.altura);
+                            Console.WriteLine($"\nNome: {temporario.nome}");
+                            Console.WriteLine($"IMC: {imc}");
+                            Console.WriteLine($"Classsificação: {IMC.classificar(imc)}");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Número inválido!");
                         break;
                 }
             }
